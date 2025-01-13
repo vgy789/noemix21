76336446661cf47d2dfeb5de21f5634566e7e6d0
vgy789 <ufers40@gmail.com>
2025-01-13 19:44:20 +0700

    gen html page for folder

--- quartz_core/quartz.config.ts
+++ quartz_core/quartz.config.ts
@@ -77,7 +77,7 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
-      //Plugin.FolderPage(),
+      Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
